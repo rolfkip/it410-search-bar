2643ab980883bce132bdbb7143eb4cc51ecda43c
Ryan <xrychx@yahoo.com>
2017-02-08 00:07:14 -0700

    working

--- search-bar.js
+++ search-bar.js
@@ -12,28 +12,39 @@
 
     class SearchBar extends HTMLElement {
 
+        static get observedAttributes() { return ['value']; }
+
         constructor() {
             super();
             let shadowRoot = this.attachShadow({mode: 'open'});
             shadowRoot.innerHTML = template;
 
-            /*this.addEventListener("keypress", function(e) {
-                
+            this.addEventListener("keyup", function(e) {
+                this.value = this.shadowRoot.querySelector('#search').value;
                 var key = e.which || e.keyCode;
                 if(key === 13) {
-                    
+                    console.log(this.value);
                 }
-            });*/
+            });
         }
 
         get value() {
-            return this.shadowRoot.querySelector('#search').getAttribute('value', newVal);
+            //return this.shadowRoot.querySelector('#search').getAttribute('value', newVal);
+            return this.getAttribute('value');
         }
 
         set value(newVal) {
             this.shadowRoot.querySelector('#search').setAttribute('value', newVal);
+            this.setAttribute('value', newVal);
+        }
+
+        attributeChangedCallback(attrName, oldValue, newValue) {
+            console.log('attr changed');
+            if(attrName == 'value') {
+                this.value = newValue;
+            }
         }
     }
 
-    window.customElements.define('search-bar', SearchBar);
+    window.customElements.define('search-input', SearchBar);
 })();
